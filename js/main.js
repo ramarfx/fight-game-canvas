6880d722baf17bcbea5adf3d2caa80a5f8a81d30
ramarfx <ramagamiing33@gmail.com>
2024-05-09 11:14:29 +0700

    parallax background

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
+import { Background } from "./background.js";
 
 window.addEventListener('load', () => {
   const canvas = document.getElementById('canvas1');
@@ -9,17 +10,22 @@ window.addEventListener('load', () => {
 
   class Game {
     constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.groundMargin = 50
-        this.player = new Player(this)
-        this.input = new InputHandler()
+      this.width = width;
+      this.height = height;
+      this.groundMargin = 50
+      this.speed = 0
+      this.maxSpeed = 3
+      this.background = new Background(this)
+      this.player = new Player(this)
+      this.input = new InputHandler()
     }
-    update(deltaTime){
+    update(deltaTime) {
+      this.background.update()
       this.player.update(this.input.keys, deltaTime)
     }
-    draw(){
-        this.player.draw(ctx)
+    draw(context) {
+      this.background.draw(context)
+      this.player.draw(context)
     }
   }
 
@@ -27,9 +33,8 @@ window.addEventListener('load', () => {
   console.log(game);
   let lastTime = 0;
 
-  function animate(timeStamp){
+  function animate(timeStamp) {
     const deltaTime = timeStamp - lastTime
-    console.log(deltaTime);
     lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     game.update(deltaTime)
